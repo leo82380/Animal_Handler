3362198b9ea66116c1e3bcc977ef399bc0d7c0ae
bigumdo <showmethe0610@gmail.com>
2024-10-31 15:34:09 +0900

    Base PatternState

---
+++ Assets/01.Scripts/LSM/Bear/FSM/State/BearPattern5State.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State/Bear/Pattern5")]
+public class BearPattern5State : State
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
