5d26481a596c2509f8f037448b610611975b6d44
leo82380 <tkdrb062@gmail.com>
2024-10-31 17:24:32 +0900

    feat: Player

---
+++ Assets/01.Scripts/Leo/Player/Player.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private PlayerMove _playerMove;
+    
+    private void FixedUpdate()
+    {
+        transform.position = _playerMove.MousePosition;
+    }
+}
---
+++ Assets/01.Scripts/Leo/Player/PlayerMove.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] private InputReaderSO _inputReaderSO;
+    public Vector3 MousePosition { get; set; }
+
+    private void Update()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(_inputReaderSO.MousePosition);
+        mousePosition.z = 0;
+        MousePosition = mousePosition;
+    }
+}
