0a5433bfa34fad2a2387f9d5e030a27837539fc2
leo82380 <tkdrb062@gmail.com>
2024-10-31 15:15:31 +0900

    feat: Add TMP

---
+++ Assets/01.Scripts/Manager/MouseWinAPIManager.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+namespace Manager.MouseWinAPI
+{
+    public static class MouseWinAPIManager
+    {
+        private const uint SPI_SETMOUSESPEED = 0x0071;
+        private const uint SPI_GETMOUSESPEED = 0x0070;
+        private const uint SPI_SETMOUSECLICKLOCK = 0x101F;
+        
+        private static int _defaultMouseSpeed;
+    
+        [DllImport("user32.dll")]
+        private static extern bool SystemParametersInfo(uint uiAction, uint uiParam, IntPtr pvParam, uint fWinIni);
+        
+        [DllImport("user32.dll")]
+        private static extern bool SystemParametersInfo(uint uiAction, uint uiParam, ref int pvParam, uint fWinIni);
+        
+        [DllImport("user32.dll")]
+        private static extern bool SetCursorPos(int X, int Y);
+    
+        /// <summary>
+        /// 마우스 고정
+        /// </summary>
+        public static void SetCursorPosInScreen(int X, int Y)
+        {
+            SetCursorPos(X, Y);
+        }
+
+        /// <summary>
+        /// 마우스 스피드 설정
+        /// </summary>
+        /// <param name="speed">마우스 속도</param>
+        public static void SetMouseSpeed(int speed)
+        {
+            if (_defaultMouseSpeed == 0)
+            {
+                _defaultMouseSpeed = GetMouseSpeed();
+            }
+            SystemParametersInfo(SPI_SETMOUSESPEED, 0, (IntPtr) speed, 0);
+        }
+        
+        public static int GetMouseSpeed()
+        {
+            int speed = 10;
+            SystemParametersInfo(SPI_GETMOUSESPEED, 0, ref speed, 0);
+            return speed;
+        }
+        
+        public static void ResetMouseSpeed()
+        {
+            SystemParametersInfo(SPI_SETMOUSESPEED, 0, (IntPtr) _defaultMouseSpeed, 0);
+        }
+    
+        /// <summary>
+        /// 마우스 입력 차단 여부
+        /// </summary>
+        /// <param name="isBlock">차단할지 여부</param>
+        public static void SetBlockInput(bool isBlock)
+        {
+            SystemParametersInfo(SPI_SETMOUSECLICKLOCK, 0, ref isBlock, 0);
+        }
+    }
+}
---
+++ Assets/01.Scripts/Manager/TestMouse.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Manager.MouseWinAPI;
+using UnityEngine;
+
+public class TestMouse : MonoBehaviour
+{
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            MouseWinAPIManager.SetCursorPosInScreen(10, 10);
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            MouseWinAPIManager.SetMouseSpeed(1);
+        }
+        else if (Input.GetKeyDown(KeyCode.F))
+        {
+            MouseWinAPIManager.SetMouseSpeed(20);
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            MouseWinAPIManager.ResetMouseSpeed();
+        }
+        else if (Input.GetKeyDown(KeyCode.G))
+        {
+            MouseWinAPIManager.SetBlockInput(true);
+        }
+        else if (Input.GetKeyDown(KeyCode.H))
+        {
+            MouseWinAPIManager.SetBlockInput(false);
+        }
+    }
+}
