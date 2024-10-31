a86a0ce646abf9ec5363a09e9a1382f0bb36c073
bigumdo <showmethe0610@gmail.com>
2024-10-31 15:18:19 +0900

    PatternBaseState

---
+++ Assets/FSMSO/01.Scripts/StateSOScript/BearIdleState.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State/BearIdle")]
+public class BearIdleState : IdleState
+{
+    public override void Enter()
+    {
+        base.Enter();
+    }
+    public override void UpdateState()
+    {
+        base.UpdateState();
+    }
+    public override void Exit()
+    {
+        base.Exit();
+    }
+}
