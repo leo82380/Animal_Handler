a0eef51f50ed1212f7d3890674ed74552f31a62f
MK <minrim0615@gmail.com>
2024-11-01 10:36:27 +0900

    paleyr asdiofsjf

--- Assets/01.Scripts/Leo/Player/Player.cs
+++ Assets/01.Scripts/Leo/Player/Player.cs
@@ -1,14 +1,31 @@
 ﻿using System;
 using System.Collections;
-using Manager.Cinemachine;
 using Manager.MouseWinAPI;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] private PlayerMove _playerMove;
     [SerializeField] private float _mouseSloowDuration = 5f;
+    private Health _health;
     
+    private void Awake()
+    {
+        _health = GetComponent<Health>();
+        _health.OnDie += OnDie;
+    }
+
+    private void OnDisable()
+    {
+        _health.OnDie -= OnDie;
+    }
+
+    private void OnDie()
+    {
+        SceneManager.LoadScene(0);
+    }
+
     private void FixedUpdate()
     {
         transform.position = _playerMove.MousePosition;
--- Assets/01.Scripts/Leo/Player/PlayerAnimation.cs
+++ Assets/01.Scripts/Leo/Player/PlayerAnimation.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Manager.Cinemachine;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -37,5 +38,10 @@ public class PlayerAnimation : MonoBehaviour
     private void OnDie()
     {
         SceneManager.LoadScene(0);
+    }
+
+    private void OnDestroy()
+    {
+        _health.OnDie -= OnDie;
     }
 }
